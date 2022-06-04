12a059f4d1301380588610a9f0ac2e3b93163b08
TravisZZZZZ <695883681@qq.com>
2022-06-04 19:40:32 +0800

    init

---
+++ src/main/java/com/travis/bopm/BopmApplication.java
@@ -0,0 +1,13 @@
+package com.travis.bopm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BopmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BopmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/travis/bopm/BopmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travis.bopm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BopmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
